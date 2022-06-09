65817ee6af54d901ab0e05900c7fe1d0eea562f9
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-09 18:43:58 -0400

    feat: implementing errorHandler on App

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { ErrorHandler } from './interfaces';
+import { httpStatus, messages } from './helpers';
 
 const app = express();
 
 app.use(express.json());
+
+app.use((err: ErrorHandler, req: Request, res: Response, _next: NextFunction) => {
+  if (err.status) {
+    return res.status(err.status).json(err.message);
+  }
+  return res.status(httpStatus.INTERNAL_ERROR).json(messages.INT_ERROR_MSG);
+});
 
 export default app;
