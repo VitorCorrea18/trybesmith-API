63e4f365947f877ffe0bd7b16a7297cd2d7888d2
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-09 19:50:11 -0400

    feat: implementing productsRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { ErrorHandler } from './interfaces';
 import { httpStatus, messages } from './helpers';
+import productsRouter from './routes/index.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRouter);
 
 app.use((err: ErrorHandler, req: Request, res: Response, _next: NextFunction) => {
   if (err.status) {
