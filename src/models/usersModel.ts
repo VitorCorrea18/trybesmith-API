fddc3dd1a6c2e542ae4798a9e3809afc69b2a47c
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-10 17:20:48 -0400

    fix: refactoring the parameters of create function in UsersModel

--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'mysql2/promise';
 import { ResultSetHeader } from 'mysql2';
 import queries from './queries';
-// import { IUser } from '../interfaces';
+import { IUser } from '../interfaces';
 
 class UserModel {
   public connection:Pool;
@@ -10,7 +10,8 @@ class UserModel {
     this.connection = connection;
   }
 
-  public async create(username:string, classe:string, level:number, password:string) {
+  public async create(newUser: IUser) {
+    const { username, classe, level, password } = newUser;
     const [{ insertId }] = await this.connection
       .execute<ResultSetHeader>(queries.createUser, [username, classe, level, password]);
     return insertId;
