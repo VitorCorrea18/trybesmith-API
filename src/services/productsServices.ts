4e1460b7c67c2ba04587a2bcc74caffc7466865c
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-10 10:29:29 -0400

    feat: implements the "create" function in ProductsService class"

--- src/services/productsServices.ts
+++ src/services/productsServices.ts
@@ -9,10 +9,15 @@ class ProductsServices {
   }
 
   public async getAll(): Promise<Iproduct[]> {
-    console.log('products service getAll');
     const products = await this.model.getAll();
 
     return products;
+  }
+
+  public async create(name:string, amount:string) {
+    const id:number = await this.model.create(name, amount);
+    const newProduct:Iproduct = { id, name, amount };
+    return newProduct;
   }
 }
 
