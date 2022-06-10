f418a7fd1251867129a23f6410f24752cc15bcb2
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-10 10:56:17 -0400

    feat: implements the "create" function in ProductsController class

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { ProductsServices } from '../services';
 import { httpStatus } from '../helpers';
+import { Iproduct } from '../interfaces';
 
 class ProductsController {
   constructor(private service = new ProductsServices()) {} // atention private
@@ -9,6 +10,16 @@ class ProductsController {
     try {
       const result = await this.service.getAll();
       return res.status(httpStatus.OK).json(result);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, amount } = req.body;
+    try {
+      const result:Iproduct = await this.service.create(name, amount);
+      return res.status(httpStatus.CREATED).json(result);
     } catch (err) {
       next(err);
     }
