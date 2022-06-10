39a7edc361cd9e4fd97686c97e6bb1742d90b863
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-10 14:23:50 -0400

    feat: implements the product route POST

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers';
+import { ProductValidation } from '../middlewares';
 
 const productsRouter = Router();
 
-const Products = new ProductsController();
+const ProductsCtrl = new ProductsController();
+const ProductVal = new ProductValidation();
 
-productsRouter.get('/', Products.getAll);
+productsRouter.get('/', ProductsCtrl.getAll);
+productsRouter.post('/', ProductVal.validateProduct, ProductsCtrl.create);
 
 export default productsRouter;
