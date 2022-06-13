0c31e27aafa89cb75902dcad03cc61fc328d2652
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-13 13:04:06 -0400

    fix: removing console.log

--- src/services/orderServices.ts
+++ src/services/orderServices.ts
@@ -18,7 +18,6 @@ class OrderServices {
       // https://github.com/tryber/sd-017-project-trybesmith/pull/30/commits/e6dde3a1c451415d58549a2c8bbd79faccadeb45
       const arrayIds = orderProducts[0].ids.split(',');
       const productsIds = arrayIds.map((id) => Number(id));
-      console.log(productsIds);
       return { ...order, productsIds };
     }));
     return result;
