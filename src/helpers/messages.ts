7aad8cd7c2a56b7a50aa6e67092de5a3e93ba995
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-13 14:26:04 -0400

    feat: implementing login function in UserServices class

--- src/helpers/messages.ts
+++ src/helpers/messages.ts
@@ -1,5 +1,6 @@
 const messages = {
   INT_ERROR_MSG: 'Internal server error',
+  USER_PASS_INVALID: 'Username or password invalid',
 };
 
 export default messages;
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,5 +1,6 @@
-import { IUser } from '../interfaces';
+import { IUser, IToken } from '../interfaces';
 import { connection, UserModel } from '../models';
+import { messages } from '../helpers';
 import { JWT } from '../utils';
 
 class UserServices {
@@ -16,6 +17,16 @@ class UserServices {
     await this.model.create(newUser);
     const token = this.jwt.generateToken(newUser);
     return token;
+  }
+
+  public async login(username:string, password:string):Promise<IToken> {
+    const foundUser = await this.model.getUserByName(username);
+    if (!foundUser[0] || foundUser[0].password !== password) {
+      const err = new Error(messages.USER_PASS_INVALID);
+      throw err;
+    }
+    const token = this.jwt.generateToken(foundUser[0]);
+    return token as IToken;
   }
 }
 
