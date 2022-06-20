6a0a3dad4081517d9d740e64c1363deef4f6d9a5
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-20 10:49:01 -0400

    feat: implementing the post /orders route

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { OrderControllers } from '../controllers';
+import { UserValidation } from '../middlewares';
 
 const orderRouter = Router();
 
 const orderCtrl = new OrderControllers();
+const userVal = new UserValidation();
 
 orderRouter.get('/', orderCtrl.getAll);
+orderRouter.post('/', userVal.validateLoggedUser, orderCtrl.create);
 
 export default orderRouter;
