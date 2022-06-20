67a3747a6a8afae67a3f0b3fca57cd2748003977
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-20 10:48:06 -0400

    feat: implementing "create" function in orderController

--- src/controllers/ordersControllers.ts
+++ src/controllers/ordersControllers.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { OrderServices } from '../services';
 import { httpStatus } from '../helpers';
-import { IOrder } from '../interfaces';
+import { IOrder, IOrderCreated } from '../interfaces';
 
 class OrderControllers {
   constructor(private services = new OrderServices()) {} // atention private
@@ -9,6 +9,16 @@ class OrderControllers {
   public getAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const result: IOrder[] = await this.services.getAll();
+      return res.status(httpStatus.OK).json(result);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    const { userId, productsIds } = req.body;
+    try {
+      const result: IOrderCreated = await this.services.create(userId, productsIds);
       return res.status(httpStatus.OK).json(result);
     } catch (err) {
       next(err);
