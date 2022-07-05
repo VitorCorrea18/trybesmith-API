97725d95704e7b7f9ff114c5de6dece38137ed7a
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-05 10:19:39 -0400

    feat: creating route POST /order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { OrderControllers } from '../controllers';
-import { UserValidation } from '../middlewares';
+import { UserValidation, OrderValidation } from '../middlewares';
 
 const orderRouter = Router();
 
 const orderCtrl = new OrderControllers();
 const userVal = new UserValidation();
+const orderVal = new OrderValidation();
 
 orderRouter.get('/', orderCtrl.getAll);
-orderRouter.post('/', userVal.validateLoggedUser, orderCtrl.create);
+orderRouter.post('/', userVal.validateLoggedUser, orderVal.validateOrder, orderCtrl.create);
 
 export default orderRouter;
