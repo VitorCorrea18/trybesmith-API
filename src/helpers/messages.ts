4fd9b21d63957434789072e910942b5d6fd5428f
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-05 10:17:47 -0400

    feat: adding new response message

--- src/helpers/messages.ts
+++ src/helpers/messages.ts
@@ -2,6 +2,7 @@ const messages = {
   INT_ERROR_MSG: 'Internal server error',
   USER_PASS_INVALID: 'Username or password invalid',
   TOKEN_NOT_FOUND: 'Token not found',
+  INVALID_TOKEN: 'Invalid token',
 };
 
 export default messages;
